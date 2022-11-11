ccc82b426474b70e151175f3dd81c76f9551554d
Nihat Akremi <nihatakremi@gmail.com>
2022-11-11 23:35:35 +0300

    ////end

---
+++ Design Patterns/Patterns/Behavioral.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns;
+
+
+//Wablon metod patterni
+
+//Gələcəkdə derived classlarin quruluwunu deyiwdirmeden alqoritmin muxtelif yerlerini alqoritmni yeniden teyin  edende
+//classda oxsar alqoritm olanda kopyalanma bas verir bu pattern kopyalanmanin qarsisin alir
+
+public abstract class AbstractClass
+{
+    public abstract void PrimitiveOperation1();
+    public abstract void PrimitiveOperation2();
+
+    // The "Template method"
+    public void TemplateMethod()
+    {
+        PrimitiveOperation1();
+        PrimitiveOperation2();
+        Console.WriteLine("");
+    }
+}
+
+public class ConcreteClassA : AbstractClass
+{
+    public override void PrimitiveOperation1()
+    {
+        Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+    }
+    public override void PrimitiveOperation2()
+    {
+        Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+    }
+}
+
+
+public class ConcreteClassB : AbstractClass
+{
+    public override void PrimitiveOperation1()
+    {
+        Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+    }
+    public override void PrimitiveOperation2()
+    {
+        Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+    }
+}
